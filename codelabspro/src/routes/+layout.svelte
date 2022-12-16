ce54e1ee1743dcdef6155ac843599407d5531614
Arunabh Das <arunabhdas@gmail.com>
2022-12-16 17:07:11 -0500

    Initialized codelabs.pro platform

---
+++ codelabspro/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">CodeLabsPro</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a class="btn btn-sm btn-ghost-surface" href="home/" rel="noreferrer">Home</a>
+				<a class="btn btn-sm btn-ghost-surface" href="about/"rel="noreferrer">About</a>
+				<a class="btn btn-sm btn-ghost-surface" href="blog/" rel="noreferrer">Blog</a>
+				<a class="btn btn-sm btn-ghost-surface" href="contact/" rel="noreferrer">Contact</a>
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ codelabspro/src/routes/page.js
@@ -0,0 +1,6 @@
+import { storePrefersDarkScheme, storeLightSwitch } from '@skeletonlabs/skeleton';
+/** @type {import('./$types').PageLoad} */
+
+export async function load() {
+
+}
